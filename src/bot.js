4810e7e6368ae85d14cac244448a677a300007f4
Jeff Sisson <jeff@bigboy.us>
2018-09-20 00:27:35 -0400

    integrating cabal-core, too fun!

--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,7 @@
-const [hub, key] = process.argv.slice(2);
-let memdb = require("memdb");
-const c = require("./chat.js");
-console.warn("mirroring chat for", key, "at", hub);
-const chat = new c("bot", memdb(), key, [hub]);
-chat.on("say", row => console.log(row.value.time, row.value.message.who));
-chat.start({ wrtc: require("wrtc") });
+// const [hub, key] = process.argv.slice(2);
+// let ram = require("random-access-memory");
+// const c = require("./chat.js");
+// console.warn("mirroring chat for", key, "at", hub);
+// const chat = new c("bot", ram(), key, [hub]);
+// chat.on("say", row => console.log(row.value.time, row.value.message.who));
+// chat.start({ wrtc: require("wrtc") });
